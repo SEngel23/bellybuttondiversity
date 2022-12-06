b6704920bb29645c8c2cd95853a67df2928a995d
SEngel23 <seanaengel@gmail.com>
2022-12-06 14:42:09 -0800

    organized files

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,7 +1,7 @@
 function init() {
     var selector = d3.select("#selDataset");
   
-    d3.json("static/js/samples.json").then((data) => {
+    d3.json("samples.json").then((data) => {
       console.log(data);
       var sampleNames = data.names;
       sampleNames.forEach((sample) => {
@@ -17,7 +17,7 @@ function init() {
   }
 
   function buildMetadata(sample) {
-    d3.json("static/js/samples.json").then((data) => {
+    d3.json("samples.json").then((data) => {
       var metadata = data.metadata;
       var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
       var result = resultArray[0];
